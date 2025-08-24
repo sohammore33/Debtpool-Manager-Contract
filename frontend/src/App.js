1bb766b60e781f12c4a74c8fac1b782ff8f7ede2
Soham More <129212461+sohammore33@users.noreply.github.com>
2025-08-24 23:54:32 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DebtPoolManager from './components/DebtPoolManager';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DebtPoolManager />
+    </div>
+  );
+}
+
+export default App;
